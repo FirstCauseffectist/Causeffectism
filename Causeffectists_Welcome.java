609d9e52aa08f0a281fb26d838bb0f8a394fe60d
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-05-24 19:59:53 -0400

    Add files via upload

---
+++ Causeffectists_Welcome.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package causeffectism.welcome;
+
+/**
+ *
+ * @author 1admin1
+ */
+public class Welcome {
+
+    public static void main(String[] args) {
+        
+        String[] greeting = new String[2];
+        greeting[0] = "Welcome to Causeffectism and All People Causing Free Openly Accessible Education";
+        greeting[1] = "by First Causeffectist Ibrahim B Rammaha";
+        
+        for (String g :greeting)
+            
+         System.out.println(g);
+    }
+}
