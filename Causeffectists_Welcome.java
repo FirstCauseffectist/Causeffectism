872905ef8d82bbce2b3255110727a84ffca60145
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-07-21 10:41:18 -0400

    Update Causeffectists_Welcome.java

--- Causeffectists_Welcome.java
+++ Causeffectists_Welcome.java
@@ -1,7 +1,3 @@
-/** For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Brian Shaw, Gordon Ramsay, 
-Jim Cramer, Ingo Rammer, Chandrasekhara Venkata Raman, Arthur Rambo, Charles Lindbergh, 
-Torakusu Yamaha, Jack Ma, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, 
-John Harvard, Muhammad Ali, and for Holy Land from Egypt's Pyramids and Alexandria Library to Syria */
 
 package causeffectism.welcome;
 
@@ -9,9 +5,13 @@ public class Welcome {
 
     public static void main(String[] args) {
         
-        String[] greeting = new String[2];
-        greeting[0] = "Welcome to Causeffectism and All People Causing Free Openly Accessible Education";
-        greeting[1] = "by First Causeffectist Ibrahim B Rammaha";
+        String[] greeting = new String[6];
+        greeting[0] = "Welcome to Causeffectism";
+        greeting[1] = "by Ibrahim Rammaha";
+        greeting[2] = "For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Brian Shaw, Gordon Ramsay, ";
+        greeting[3] = "Jim Cramer, Ingo Rammer, Chandrasekhara Venkata Raman, Arthur Rambo, Charles Lindbergh, ";
+        greeting[4] = "Torakusu Yamaha, Jack Ma, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, ";
+        greeting[5] = "John Harvard, Muhammad Ali, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria";
         
         for (String g :greeting)
             
