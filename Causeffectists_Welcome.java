2d3b0294d3f03e4f9f9a720eab7f017d978eb0b1
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-07-23 20:10:36 -0400

    Update Causeffectists_Welcome.java

--- Causeffectists_Welcome.java
+++ Causeffectists_Welcome.java
@@ -5,13 +5,16 @@ public class Welcome {
 
     public static void main(String[] args) {
         
-        String[] greeting = new String[6];
+        String[] greeting = new String[9];
         greeting[0] = "Welcome to Causeffectism";
         greeting[1] = "by Ibrahim Rammaha";
         greeting[2] = "For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Brian Shaw, Gordon Ramsay, ";
-        greeting[3] = "Jim Cramer, Ingo Rammer, Chandrasekhara Venkata Raman, Arthur Rambo, Charles Lindbergh, ";
+        greeting[3] = "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Arthur Rambo, Charles Lindbergh, ";
         greeting[4] = "Torakusu Yamaha, Jack Ma, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, ";
         greeting[5] = "John Harvard, Muhammad Ali, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria";
+        greeting[6] = "Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, ";
+        greeting[7] = "volcanoes, tornadoes, thunderstorms, hurricanes? I know what it feels like to be raped as an orphan and made Causeffectism for ";
+        greeting[8] = "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers";
         
         for (String g :greeting)
             
