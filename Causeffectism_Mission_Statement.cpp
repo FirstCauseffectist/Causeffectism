bf89f073315d5e0675d69ae5cf606f22a414ac7a
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-08-04 22:06:29 -0400

    Update Causeffectism_Mission_Statement.cpp

--- Causeffectism_Mission_Statement.cpp
+++ Causeffectism_Mission_Statement.cpp
@@ -7,11 +7,11 @@ int main()
     cout << "Welcome to Causeffectism";
     cout << " by Ibrahim Rammaha.";
     cout << " For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, ";
-    cout << "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Arthur Rambo, Charles Lindbergh, ";
-    cout << "Torakusu Yamaha, Jack Ma, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, ";
+    cout << "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Charles Lindbergh, ";
+    cout << "Torakusu Yamaha, Jack Ma, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
     cout << "John Harvard, Muhammad Ali, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria.";
     cout << " Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
-    cout << "volcanoes, tornadoes, tsunamis, hurricanes? I know what it feels like to be raped and robbed as an orphan and made Causeffectism for ";
+    cout << "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and homelessly robbed as an orphan and made Causeffectism for ";
     cout << "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers...";
 
 }
