7b50931ca07e56125c6ca053a6e09c44352fa4ef
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-08-04 22:07:26 -0400

    Update Causeffectists_Welcome.java

--- Causeffectists_Welcome.java
+++ Causeffectists_Welcome.java
@@ -6,14 +6,14 @@ public class Welcome {
     public static void main(String[] args) {
         
         String[] greeting = new String[9];
-        greeting[0] = "Welcome to Causeffectism";
+       greeting[0] = "Welcome to Causeffectism";
         greeting[1] = "by Ibrahim Rammaha";
         greeting[2] = "For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, ";
-        greeting[3] = "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Arthur Rambo, Charles Lindbergh, ";
-        greeting[4] = "Torakusu Yamaha, Jack Ma, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, ";
+        greeting[3] = "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Charles Lindbergh, ";
+        greeting[4] = "Torakusu Yamaha, Jack Ma, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
         greeting[5] = "John Harvard, Muhammad Ali, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria";
         greeting[6] = "Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
-        greeting[7] = "volcanoes, tornadoes, tsunamis, hurricanes? I know what it feels like to be raped and robbed as an orphan and made Causeffectism for ";
+        greeting[7] = "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and homelessly robbed as an orphan and made Causeffectism for ";
         greeting[8] = "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers";
         
         for (String g :greeting)
