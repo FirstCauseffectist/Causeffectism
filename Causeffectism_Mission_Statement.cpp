63f683dba7cf9ceaa3dbb520d19f7dcc32145318
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-08-06 11:15:21 -0400

    Update Causeffectism_Mission_Statement.cpp

--- Causeffectism_Mission_Statement.cpp
+++ Causeffectism_Mission_Statement.cpp
@@ -13,5 +13,5 @@ int main()
     cout << " Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
     cout << "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and homelessly robbed as an orphan and made Causeffectism for ";
     cout << "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers...";
-
+    cout << "If you are homeless McDonalds, Burger King, Dairy Queen Grill and Chill, Walmart Supermarket, and local storage rental are the cheapest places in America.";
 }
