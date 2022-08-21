1b2c11c0878a4388270a5328fddaed0874265d43
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-08-21 06:01:50 -0400

    Update Causeffectists_Welcome.java

--- Causeffectists_Welcome.java
+++ Causeffectists_Welcome.java
@@ -14,8 +14,8 @@ public class Welcome {
         greeting[5] = "John Harvard, Muhammad Ali, Thomas Edison, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria";
         greeting[6] = "Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
         greeting[7] = "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and forced to be homeless then robbed as an orphan and made Causeffectism for ";
-        greeting[8] = "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers. Getting sick of sodomizers profiting on porn and pedophile gays getting college scholarships.";
-        greeting[9] = "Get ready for UNITED STATES OF ARABIA, North America, Antarctica, Oceania, SOUTH AMERICA, AFRICA, EURASIA, Pacific Islands. If you are homeless McDonalds, Burger King, Dairy Queen Grill and Chill, Walmart Supermarket, and local storage rental are the cheapest places in America.";       
+        greeting[8] = "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers. Getting sick of sodomizers profiting through porn and pedophile gays getting college scholarships.";
+        greeting[9] = "US made internet and only democracy in world so get ready for UNITED STATES OF ARABIA, North America, Antarctica, Oceania, SOUTH AMERICA, AFRICA, EURASIA, Pacific Islands. If you are homeless McDonalds, Burger King, Dairy Queen Grill and Chill, Walmart Supermarket, and local storage rental are the cheapest places in America.";       
         for (String g :greeting)
             
          System.out.println(g);
