3be18707afcd206474a2807d61aaf65c9f1dc450
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-08-21 06:00:16 -0400

    Update Causeffectism_Mission_Statement.cpp

--- Causeffectism_Mission_Statement.cpp
+++ Causeffectism_Mission_Statement.cpp
@@ -11,7 +11,7 @@ int main()
     cout << "Torakusu Yamaha, Jack Ma, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
     cout << "John Harvard, Muhammad Ali, Thomas Edison, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria.";
     cout << " Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
-    cout << "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and homelessly robbed as an orphan and made Causeffectism for ";
-    cout << "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers...";
-    cout << "If you are homeless McDonalds, Burger King, Dairy Queen Grill and Chill, Walmart Supermarket, and local storage rental are the cheapest places in America.";
+    cout << "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and forced to be homeless and robbed as an orphan and made Causeffectism for ";
+    cout << "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers. Getting sick of sodomizers profiting through porn and pedophile gays getting college scholarships.";
+    cout << "US made internet and only democracy in world so get ready for UNITED STATES OF ARABIA, North America, Antarctica, Oceania, SOUTH AMERICA, AFRICA, EURASIA, Pacific Islands. If you are homeless McDonalds, Burger King, Dairy Queen Grill and Chill, Walmart Supermarket, and local storage rental are the cheapest places in America.";
 }
