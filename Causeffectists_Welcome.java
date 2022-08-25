0cae32a892a7b2ef17d197f9414a8d2978511964
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-08-24 23:14:14 -0400

    Update Causeffectists_Welcome.java

--- Causeffectists_Welcome.java
+++ Causeffectists_Welcome.java
@@ -7,7 +7,7 @@ public class Welcome {
         
         String[] greeting = new String[10];
         greeting[0] = "Welcome to Causeffectism";
-        greeting[1] = "by First Causeffectist SHETLM Grandmaster Expert, Saint, Sheikh, Brahma, Rabbi, Shifu Ibrahim Rammaha.";
+        greeting[1] = "by First Causeffectist SHETLM Grandmaster Expert, Saint, Sheikh, Brahman, Rabbi, Shifu Ibrahim Rammaha.";
         greeting[2] = "For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, ";
         greeting[3] = "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Charles Lindbergh, ";
         greeting[4] = "Torakusu Yamaha, Jack Ma, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
