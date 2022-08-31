a42261a8470f5077dc08baac8f89822e8f1c279b
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-08-30 21:44:18 -0400

    Update Causeffectists_Welcome.java

--- Causeffectists_Welcome.java
+++ Causeffectists_Welcome.java
@@ -10,7 +10,7 @@ public class Welcome {
         greeting[1] = "by First Causeffectist SHETLM Grandmaster Expert, Saint, Sheikh, Brahman, Rabbi, Shifu, Kru, Sensei, Space Guardian Angel Ibrahim Rammaha.";
         greeting[2] = "For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, ";
         greeting[3] = "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Charles Lindbergh, ";
-        greeting[4] = "Torakusu Yamaha, Jack Ma, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
+        greeting[4] = "Torakusu Yamaha, Jack Ma, Stephen Wolfram, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
         greeting[5] = "John Harvard, Muhammad Ali, Thomas Edison, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria";
         greeting[6] = "Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
         greeting[7] = "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and forced to be homeless then robbed as an orphan and made Causeffectism for ";
