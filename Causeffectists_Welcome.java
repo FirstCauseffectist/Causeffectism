0a95a6f9b91ea65cd0b215463e5389266d91d126
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-09-01 00:05:52 -0400

    Update Causeffectists_Welcome.java

--- Causeffectists_Welcome.java
+++ Causeffectists_Welcome.java
@@ -13,7 +13,7 @@ public class Welcome {
         greeting[4] = "Torakusu Yamaha, Jack Ma, Stephen Wolfram, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
         greeting[5] = "John Harvard, Muhammad Ali, Thomas Edison, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria";
         greeting[6] = "Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
-        greeting[7] = "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and forced to be homeless then robbed as an orphan and made Causeffectism for ";
+        greeting[7] = "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and forced to be homeless then robbed as an orphan and made Causeffectism and 24/7 public library shelters for ";
         greeting[8] = "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers. Getting sick of sodomizers profiting through porn and pedophile gays getting college scholarships.";
         greeting[9] = "US made internet and only democracy in world so get ready for new states UNITED STATES OF America, Arabia, Antarctica, Commonwealth Britain, Iran, Zimbabwe, Russia, Malaysia, and Mauritius based on their amazing government policies including punishing sodomy and other crimes. If you are homeless McDonalds, Burger King, Dairy Queen Grill and Chill, Walmart Supermarket, and local storage rental are the cheapest places in America";       
         for (String g :greeting)
