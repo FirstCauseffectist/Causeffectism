f7a50c3fae0bcceac1fa747417c691bcf8be7e20
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-11-08 12:38:53 -0500

    Update Causeffectism_Mission_Statement.cpp

--- Causeffectism_Mission_Statement.cpp
+++ Causeffectism_Mission_Statement.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Causeffectism: Under God With Liberty and Justice for All Anti-Sinning God Knowing Muslims and Christians Providing Free Education";
+     cout << "Causeffectism: Under God With Liberty and Justice for All Anti-Sinning God Knowing Muslims and Christians Providing Free Education";
     cout << " by First Causeffectist SHETLM Grandmaster Expert, Sheikh Priest, Space Guardian Angel Ibrahim Rammaha.";
     cout << " For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, Khabib Nurmagomedov, ";
     cout << "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Salman Amin Khan, ";
@@ -12,6 +12,8 @@ int main()
     cout << "John Harvard, Muhammad Ali, Thomas Edison, Jawed Karim, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria.";
     cout << " Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
     cout << "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and forced to be homeless and robbed as an orphan and made Causeffectism and public 24/7 library shelters for ";
-    cout << "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers. Getting sick of sodomizers profiting through porn and pedophile gays getting funds from government taxes.";
-    cout << "US made internet and only democracy in world so get ready for new states UNITED STATES OF America, Arabia, Antarctica, Commonwealth Britain, Iran, Zimbabwe, Russia, Malaysia, and Mauritius based on their amazing government policies including punishing sodomy and other crimes. If you are homeless McDonalds, Burger King, Dairy Queen Grill and Chill, Walmart Supermarket, and local storage rental are the cheapest places in America.";
+    cout << "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers.";
+    cout << "Getting sick of sodomizers profiting through porn and pedophile gays getting funds from government taxes.";
+    cout << "US made internet and only democracy in world so get ready for new states UNITED STATES OF America, Arabia, Antarctica, Commonwealth Britain, Iran, Zimbabwe, Russia, Malaysia,";
+    cout << " and Mauritius based on their amazing government policies including punishing sodomy and other crimes. If you are homeless Walmart Supermarket and local storage rental are the cheapest places in America.";
 }
