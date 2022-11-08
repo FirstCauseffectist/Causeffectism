2a9cd657805ebdd647a058c3537dd4aa8aab2ab5
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-11-08 12:33:27 -0500

    Update Causeffectism_Mission_Statement.cpp

--- Causeffectism_Mission_Statement.cpp
+++ Causeffectism_Mission_Statement.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Welcome to Causeffectism";
-    cout << " by First Causeffectist SHETLM Grandmaster Expert, Sheikh Priest Kru Sensei, Space Guardian Angel Ibrahim Rammaha.";
-    cout << " For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, ";
-    cout << "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Charles Lindbergh, ";
+    cout << "Causeffectism: Under God With Liberty and Justice for All Anti-Sinning God Knowing Muslims and Christians Providing Free Education";
+    cout << " by First Causeffectist SHETLM Grandmaster Expert, Sheikh Priest, Space Guardian Angel Ibrahim Rammaha.";
+    cout << " For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, Khabib Nurmagomedov, ";
+    cout << "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Salman Amin Khan, ";
     cout << "Torakusu Yamaha, Jack Ma, Stephen Wolfram, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
-    cout << "John Harvard, Muhammad Ali, Thomas Edison, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria.";
+    cout << "John Harvard, Muhammad Ali, Thomas Edison, Jawed Karim, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria.";
     cout << " Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
     cout << "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and forced to be homeless and robbed as an orphan and made Causeffectism and public 24/7 library shelters for ";
     cout << "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers. Getting sick of sodomizers profiting through porn and pedophile gays getting funds from government taxes.";
