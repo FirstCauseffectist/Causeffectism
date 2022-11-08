87a0c98676bb14a2da644ef5d2bdf5d6571a89d6
First Causeffectist <60399100+FirstCauseffectist@users.noreply.github.com>
2022-11-08 12:44:43 -0500

    Update Causeffectists_Welcome.java

--- Causeffectists_Welcome.java
+++ Causeffectists_Welcome.java
@@ -6,16 +6,16 @@ public class Welcome {
     public static void main(String[] args) {
         
         String[] greeting = new String[10];
-        greeting[0] = "Welcome to Causeffectism";
-        greeting[1] = "by First Causeffectist SHETLM Grandmaster Expert, Sheikh Priest Kru Sensei, Space Guardian Angel Ibrahim Rammaha.";
-        greeting[2] = "For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, ";
-        greeting[3] = "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Charles Lindbergh, ";
+        greeting[0] = "Causeffectism: Under God With Liberty and Justice for All Anti-Sinning God Knowing Muslims and Christians Providing Free Education";
+        greeting[1] = "by First Causeffectist SHETLM Grandmaster Expert, Sheikh Priest, Space Guardian Angel Ibrahim Rammaha.";
+        greeting[2] = "For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, Khabib Nurmagomedov, ";
+        greeting[3] = "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Salman Amin Khan, ";
         greeting[4] = "Torakusu Yamaha, Jack Ma, Stephen Wolfram, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
-        greeting[5] = "John Harvard, Muhammad Ali, Thomas Edison, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria";
+        greeting[5] = "John Harvard, Muhammad Ali, Thomas Edison, Jawed Karim, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria";
         greeting[6] = "Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
         greeting[7] = "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and forced to be homeless then robbed as an orphan and made Causeffectism and 24/7 public library shelters for ";
         greeting[8] = "free education providers and orphans to accomplish good things independently and eat evil harmful pleasure enforcers. Getting sick of sodomizers profiting through porn and pedophile gays getting funds from government taxes.";
-        greeting[9] = "US made internet and only democracy in world so get ready for new states UNITED STATES OF America, Arabia, Antarctica, Commonwealth Britain, Iran, Zimbabwe, Russia, Malaysia, and Mauritius based on their amazing government policies including punishing sodomy and other crimes. If you are homeless McDonalds, Burger King, Dairy Queen Grill and Chill, Walmart Supermarket, and local storage rental are the cheapest places in America";       
+        greeting[9] = "US made internet and only democracy in world so get ready for new states UNITED STATES OF America, Arabia, Antarctica, Commonwealth Britain, Iran, Zimbabwe, Russia, Malaysia, and Mauritius based on their amazing government policies including punishing sodomy and other crimes. If you are homeless Walmart Supermarket and local storage rental are the cheapest places in America";       
         for (String g :greeting)
             
          System.out.println(g);
