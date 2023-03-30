370ccb2f608b3cb84d384b2e80a599b07552ff5a
First Causeffectist Allahu Akbar <60399100+FirstCauseffectist@users.noreply.github.com>
2023-03-29 19:36:52 -0700

    Update Causeffectism_Mission_Statement.cpp

--- Causeffectism_Mission_Statement.cpp
+++ Causeffectism_Mission_Statement.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-     cout << "Causeffectism: Under God With Liberty and Justice for All Anti-Sinning God Knowing Muslims and Christians Providing Free Education under United States of Christians Muslims";
-    cout << " by First Causeffectist SHETLM Grandmaster Expert, Sheikh Priest, Space Guardian Angel Ibrahim Rammaha.";
-    cout << " For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, Khabib Nurmagomedov, ";
+    cout << "Causeffectism: Under God With Liberty and Justice for All Anti-Sinning God Knowing Muslims and Christians Providing Free Education under United States of Christians Muslims. Future will be constitutional christian muslim theocracy and open access education. You're welcome Vatican Pope and royals so don't end up like Tsars serial murdered";
+    cout << " by First Causeffectist SHETLM Grandmaster Expert, Universe's Priest Imam, Space Guardian Angel Ibrahim Rammaha.";
+    cout << " For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, Islam Ramazanovich Makhachev, Khabib Nurmagomedov, ";
     cout << "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Salman Amin Khan, ";
-    cout << "Torakusu Yamaha, Jack Ma, Stephen Wolfram, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
+    cout << "Torakusu Yamaha, Jack Ma, Stephen Wolfram, Isaac Newton, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
     cout << "John Harvard, Muhammad Ali, Thomas Edison, Jawed Karim, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria.";
     cout << " Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
     cout << "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and forced to be homeless and robbed as an orphan and made Causeffectism and public 24/7 library shelters for ";
