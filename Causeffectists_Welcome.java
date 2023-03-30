3562b528dc70cefc47a9dc7387c1fae34c56e8a8
First Causeffectist Allahu Akbar <60399100+FirstCauseffectist@users.noreply.github.com>
2023-03-29 19:42:28 -0700

    Update Causeffectists_Welcome.java

--- Causeffectists_Welcome.java
+++ Causeffectists_Welcome.java
@@ -6,11 +6,11 @@ public class Welcome {
     public static void main(String[] args) {
         
         String[] greeting = new String[10];
-        greeting[0] = "Causeffectism: Under God With Liberty and Justice for All Anti-Sinning God Knowing Muslims and Christians Providing Free Education under United States of Christians Muslims";
-        greeting[1] = "by First Causeffectist SHETLM Grandmaster Expert, Sheikh Priest, Space Guardian Angel Ibrahim Rammaha.";
-        greeting[2] = "For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, Khabib Nurmagomedov, ";
+        greeting[0] = "Causeffectism: Under God With Liberty and Justice for All Anti-Sinning God Knowing Muslims and Christians Providing Free Education under United States of Christians Muslims. Future will be constitutional christian muslim theocracy and open access education. You're welcome Vatican Pope and royals so don't end up like Tsars serial murdered";
+        greeting[1] = "by First Causeffectist SHETLM Grandmaster Expert, Universe's Priest Imam, Space Guardian Angel Ibrahim Rammaha.";
+        greeting[2] = "For my heroes Cain Ramirez Velasquez, Ramzan Kadyrov, Murad and Alaverdi Ramazanov, Brian Shaw, Gordon Ramsay, Islam Ramazanovich Makhachev, Khabib Nurmagomedov, ";
         greeting[3] = "Jim Cramer, Ingo Rammer, Abraham Lincoln, Chandrasekhara Venkata Raman, Alexandra Elbakyan, Arthur Rambo, Salman Amin Khan, ";
-        greeting[4] = "Torakusu Yamaha, Jack Ma, Stephen Wolfram, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
+        greeting[4] = "Torakusu Yamaha, Jack Ma, Stephen Wolfram, Isaac Newton, Henry Ford, Muhammad ibn Musa al-Khwarizmi, Elizabeth Warren, Ilhan Omar, Tim and Paul Samaras, ";
         greeting[5] = "John Harvard, Muhammad Ali, Thomas Edison, Jawed Karim, and for Holy Land from Egyptian Pyramids and Alexandria Library to Syria";
         greeting[6] = "Do you want to eat pedophile bisexual criminals raping and sodomizing good orphans and want to own caves, pyramids, libraries, earthquakes, thunderstorms, ";
         greeting[7] = "volcanoes, tornadoes, tsunamis, hurricanes, blackholes? I know what it feels like to be raped and forced to be homeless then robbed as an orphan and made Causeffectism and 24/7 public library shelters for ";
